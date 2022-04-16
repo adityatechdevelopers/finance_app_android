67f51130ceb4a80ffb450728406278a30a84c076
Aditya Tech Developers <adityatechdevelopers@gmail.com>
2022-04-16 14:29:57 +0530

    initial commit

--- app/src/main/java/com/developerstring/financemanagementapp/screen/AccountScreen.kt
+++ app/src/main/java/com/developerstring/financemanagementapp/screen/AccountScreen.kt
@@ -3,9 +3,14 @@ package com.developerstring.financemanagementapp.screen
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.bumptech.glide.Glide
+import com.developerstring.financemanagementapp.R
 import com.developerstring.financemanagementapp.databinding.ActivityAccountScreenBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 
 class AccountScreen : AppCompatActivity() {
@@ -56,15 +61,33 @@ class AccountScreen : AppCompatActivity() {
             builder.setTitle("Confirm Logout")
             builder.setMessage("Are you sure you want to logout")
             builder.setPositiveButton("Logout") { dialog, which ->
-                mAuth.signOut()
-                val signInScreenIntent = Intent(this@AccountScreen, SignInScreen::class.java)
-                startActivity(signInScreenIntent)
-                finish()
+                logOut()
             }
             builder.setNegativeButton("Cancel") { dialog, which ->
 
             }
             builder.show()
+        }
+    }
+    private fun logOut(){
+        // get the google account
+        val googleSignInClient: GoogleSignInClient
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        // sign out of your account
+        mAuth.signOut()
+        googleSignInClient.signOut().addOnSuccessListener {
+            val signInScreenIntent = Intent(this@AccountScreen, SignInScreen::class.java)
+            startActivity(signInScreenIntent)
+            finish()
+        }.addOnFailureListener {
+            Toast.makeText(this@AccountScreen,"Error! while logging out",Toast.LENGTH_SHORT).show()
         }
     }
 }
